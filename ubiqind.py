dae6973513e15ac2df68423c1c03904b6b8f8742
Maximilian Dore <maximilian.dore@hexhex.net>
2018-07-06 12:43:39 +0200

    Moves agent logic in own class

--- ubiqind.py
+++ ubiqind.py
@@ -1,5 +1,8 @@
 import argparse
 import random
+import numpy
+import math 
+
 
 # Read the command line arguments
 parser = argparse.ArgumentParser()
@@ -10,18 +13,27 @@ parser.add_argument("-v", help="Show debugging information", action='store_true'
 args = parser.parse_args()
 
 
-cur_round = 1
+# Model parameters not given as command line arguments
+args.pulls = 1000
+args.ops1 = .4 #.499
+args.ops2 = .5
+args.maxprior = 2 
 
-# The main loop
-sucs = []
-fails = []
-while cur_round <= args.rounds:
-    if args.v:
-        print("Playing round " + str(cur_round))
-    
-    # Initializing all agents and parameters
-    agents = []
-    for i in range(args.agents):
+
+class Agent:
+    """
+    The agent class with helpers for initializing 
+    and updating an individual scientist
+    """
+    def __init__(self, id):
+        self.id = id
+        a1 = args.maxprior - random.uniform(0, args.maxprior)
+        b1 = args.maxprior - random.uniform(0, args.maxprior)
+        a2 = args.maxprior - random.uniform(0, args.maxprior)
+        b2 = args.maxprior - random.uniform(0, args.maxprior)
+        self.a = [a1,a2]
+        self.b = [a1+b1, a2+b2]
+
         if args.network == "complete":
             peers = list(range(0,i)) + list(range(i+1,args.agents))
         elif args.network == "circle":
@@ -36,33 +48,64 @@ while cur_round <= args.rounds:
             for j in range(args.agents):
                 if i != j and float(args.network) > random.random():
                     peers.append(j)
+        self.peers = peers
+        if args.v:
+            print("Init" + str(self)) 
 
-        agents.append({
-            'prob_new_better': random.random(),
-            'peers': peers,
-        })
+
+    def __str__(self):
+        return "Agent " + str(self.id) + ": " + str(self.a) + ", " + str(self.b) + ", " + str(self.peers)  
+
+    def get_posterior(self):
+        return [x/y for x, y in zip(self.a, self.b)]
+
+    def snd_better(self):
+        return True
+        # return probsnd > .5
+
+    def snd_confident(self):
+        return True
+        # return probsnd > .9999
+
+
+cur_round = 1
+sucs = []
+fails = []
+
+# The main loop
+while cur_round <= args.rounds:
     if args.v:
-        print("Agents initialized:")
-        for i in range(args.agents):
-            print(str(i) + ": " + str(agents[i]))
+        print("Playing round " + str(cur_round))
+    
+    # Initializing all agents and parameters
+    agents = []
+    for i in range(args.agents):
+        agents.append(Agent(i))
 
     num_gens = 1
     while True:
         for a in agents:
-            a['prob_new_better'] = a['prob_new_better'] + random.random()/10
-            # pursue method, depending which they find better
+            ops = args.ops1 if not a.snd_better() else args.ops2
+            sucpulls = round(numpy.random.normal(args.pulls*ops, math.sqrt(args.pulls*ops*(1-ops))))
+            if args.v:
+                print("Agent " + str(a.id) + " pulled: " + str(sucpulls))
+
+            # For Testing:
+            
+            # a['prob_new_better'] = a['prob_new_better'] + random.random()/10
             # update belief according to result of resp. experiment 
             # receive payoff
+            # cf Bala Goyal p. 10
 
         # agents meet and discuss results
-     
-        if all(a['prob_new_better'] < .5 for a in agents):
+
+        if all(not a.snd_better() for a in agents):
             if args.v:
                 print("All agents believe inferior method is better after generation " + str(num_gens))
             fails.append(num_gens)
             break
 
-        if all(a['prob_new_better'] > .9999 for a in agents):
+        if all(a.snd_confident() for a in agents):
             if args.v:
                 print("All agents are confident superior method is better after generation " + str(num_gens))
             sucs.append(num_gens)
